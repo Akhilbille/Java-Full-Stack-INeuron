077de87280370cfda974628bdda9445157cf606b
Akhil Bille <akhilgowd829@gmail.com>
2023-01-30 19:59:45 +0530

    H,X pattern files added

--- DataType.java
+++ DataType.java
@@ -6,6 +6,18 @@ class DataType
 		float num2 = 13f;
 		int res  = 	 (int)(num/num2);
 		System.out.println(res);
+		byte b1 = (byte) 148;
+		System.out.println(b1);
+		int ch = '?';
+		System.out.println(ch);
+		char ch1= (char)200000000000L;
+		System.out.println(ch1);
+
+		 args[0] =  "123";
+		// System.out.println(ab);
+
+
+
 		
 	}
 	
---
+++ HPattern.java
@@ -0,0 +1,20 @@
+class HPattern 
+{
+    public static void main(String[] args)
+    {
+        int n=6;
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<n;j++)
+            {
+                if (j==0 || j==(n-1) || i==(n%2==0?n/2:(n-1)/2))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
+    
+}
