ff7db2048b73058048302e30930f95da0b2a3c06
Akhil Bille <akhilgowd829@gmail.com>
2023-01-30 23:07:33 +0530

    Adding Datatype 4 file

---
+++ DataType4.java
@@ -0,0 +1,8 @@
+class DataType4{
+    public static void main(String[] args){
+        char c ='Z';
+        int n = 4;
+        long l = c;
+        System.out.println(l);
+    }
+}
---
+++ DataTypeSize.java
@@ -0,0 +1,26 @@
+ class DataTypeSize {
+
+    public static void main(String [] args)
+    {
+
+        System.out.println("Integer ");
+        System.out.println("Integer Min Value : " + Integer.MIN_VALUE);
+        System.out.println("Integer MAX Value : " + Integer.MAX_VALUE);
+        System.out.println("Integer Bytes : " + Integer.BYTES);
+        System.out.println("========================================" );
+        System.out.println("Char ");
+        System.out.println("Character Min Value : " + Character.MIN_VALUE);
+        System.out.println("Character Max Value : " + Character.MAX_VALUE);
+        System.out.println("Character Bytes : " + Character.BYTES);
+        System.out.println("========================================" );
+        System.out.println("Bytes ");
+        System.out.println("Bytes Min Value : " + Byte.MIN_VALUE);
+        System.out.println("Bytes Max Value : " + Byte.MAX_VALUE);
+        System.out.println("Bytes Bytes : " + Byte.BYTES);
+        System.out.println("Bytes Bytes : " + Byte.TYPE);
+        System.out.println("Bytes Bytes : " + Byte.SIZE);
+        System.out.println("========================================" );
+        
+    }
+    
+}
