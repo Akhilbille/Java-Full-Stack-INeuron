6fc91372ad0b6d3c62abff49009c7c648bd4f291
Akhil Bille <akhilgowd829@gmail.com>
2023-02-06 12:39:40 +0530

    Experminet file added

---
+++ Experiment.java
@@ -0,0 +1,18 @@
+class Experiment {
+    public static void main(String [] args){
+        // int b;        
+        // System.out.println(b+1);
+
+        // double f = 32.233233;
+        // System.out.println(f); 
+
+        // byte b = (byte) 1234.00;
+        // System.out.println(b);;;;;
+        
+        int i = 5;
+        if (i<5);; 
+
+
+    }
+    
+}
