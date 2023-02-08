c4df4e2006852c25400f4d5c9d240d25ed17c3c8
Akhil Bille <akhilgowd829@gmail.com>
2023-02-09 01:06:54 +0530

    Adding Assignments

---
+++ Student.java
@@ -0,0 +1,17 @@
+ class Student {
+
+    int sid;
+    int sname;
+
+    void play()
+    {
+        System.out.println("Playing Cricket");
+    }
+    void sleep(){
+        System.out.println("Sleeping.....");
+    }
+
+
+
+
+}
